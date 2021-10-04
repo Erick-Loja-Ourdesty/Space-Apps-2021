b12e9299f70515248b9631929d8b7443503e275a
Erick Arturo Loja Ponce <66175593+Erick-Loja-Ourdesty@users.noreply.github.com>
2021-10-04 02:32:02 -0500

    Create servicios.java

---
+++ servicios.java
@@ -0,0 +1,14 @@
+package com.example.mitienda;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class servicios extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_servicios);
+    }
+}
