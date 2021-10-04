356e4334b3caa343ef0d19a49cf4434934a362fc
Erick Arturo Loja Ponce <66175593+Erick-Loja-Ourdesty@users.noreply.github.com>
2021-10-04 02:27:46 -0500

    Create contactanos.java

---
+++ contactanos.java
@@ -0,0 +1,14 @@
+package com.example.mitienda;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class contactanos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contactanos);
+    }
+}
