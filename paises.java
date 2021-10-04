7896812728d3f1b53a5cca476b1c3306734a0634
Erick Arturo Loja Ponce <66175593+Erick-Loja-Ourdesty@users.noreply.github.com>
2021-10-04 02:31:34 -0500

    Create paises.java

---
+++ paises.java
@@ -0,0 +1,14 @@
+package com.example.mitienda;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class paises extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_paises);
+    }
+}
